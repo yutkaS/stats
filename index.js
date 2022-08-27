8094ef2fd0651be1f2866575f27c5e3720fe34a5
nametochange <m.ponomarenko@studyworld.io>
2022-08-27 22:44:12 +0300

    change

--- index.js
+++ index.js
@@ -7,20 +7,38 @@ const chatIdTest = -787142727;
 const bottleChatId = -1001537695968;
 const statisticChatId = -643353109;
 const messageToTrashTalk = 20;
-const timeToTrashTalk = 60 * 5;
+const timeToInterval = 1000 * 60 * 5;
 
 const isProd = true;
 const file = isProd ? "stats.json" : "test.json"
 const chat = isProd ? bottleChatId : chatIdTest;
 
-const sendMessage = async (text) => fetch(`https://api.telegram.org/bot${myToken}/sendMessage?chat_id=${statisticChatId}&text=${text}`);
+// счетчик последнего обработанного сообщения
+let lastReaded = 0;
+
+const sendStat = () => {
+    const statsJson = fs.readFileSync(file, 'utf8');
+    fetch(`https://api.telegram.org/bot${myToken}/sendMessage?chat_id=${statisticChatId}&text=${statsJson}`);
+};
+
+const sendMessage = (text) => {
+    fetch(`https://api.telegram.org/bot${myToken}/sendMessage?chat_id=${chat}&text=${text}`);
+};
+
+const handleDayEnd = () => {
+    const statsJson = fs.readFileSync(file, 'utf8');
+    const stat = JSON.parse(statsJson);
+
+    if (stat[0].hasTrashTalkToday) sendMessage('Хорошая работа, всем миска риса и кошко жена!');
+    else sendMessage('Партия недовольна вами, всем -100 рейтинга!');
+
+    fs.writeFileSync(file, `[{"trashTalkActive": "false", "hasTrashTalkToday": "false", "isProd": "${isProd}"}]`, 'utf8');
+}
+
 
 const getUpdates = async () => fetch(`https://api.telegram.org/bot${myToken}/getUpdates?offset=-100`)
     .then((e) => e.json())
     .then(({result}) => result);
-
-// счетчик последнего обработанного сообщения
-let lastReaded = 0;
 
 // получить индекс юзера в главном массиве
 const getUserIndex = (id) => {
@@ -72,6 +90,7 @@ const startTrashTalk = () => {
     const statsJson = fs.readFileSync(file, 'utf8');
     const stats = JSON.parse(statsJson);
 
+    stats[0].hasTrashTalkToday = true;
     stats[0].trashTalkActive = true;
 
     fs.writeFileSync(file, JSON.stringify(stats), 'utf8');
@@ -81,11 +100,10 @@ const startTrashTalk = () => {
 // проверить нужен ли треш ток
 const checkTrashTalk = (updates) => {
     const updatesCount = updates.length;
-
     if (messageToTrashTalk > updatesCount || !updates[updatesCount - 1].message || !updates[updatesCount - messageToTrashTalk].message) return;
     if (
-        (updates[updatesCount - 1].message.date - updates[updatesCount - messageToTrashTalk].message.date) < timeToTrashTalk &&
-        Date.now() / 1000 - updates[updatesCount - 1].message.date < 60
+        (updates[updatesCount - 1].message.date - updates[updatesCount - messageToTrashTalk].message.date) * 1000 < timeToInterval &&
+        Date.now() / 1000 - updates[updatesCount - 1].message.date < 60 * 5
     ) startTrashTalk();
     else stopTrashTalk()
 };
@@ -141,17 +159,15 @@ const update = (update) => {
 
 // интервал
 const handleInterval = async () => {
-
     const updates = await getUpdates();
-    await checkTrashTalk(updates);
-    await updates.map(update);
-
-    const statsJson = fs.readFileSync(file, 'utf8');
-    await sendMessage(statsJson);
+    checkTrashTalk(updates);
+    updates.map(update);
+    sendStat();
+    if (new Date().getHours() > 22) handleDayEnd();
 };
 
-fs.writeFileSync(file, `[{"trashTalkActive": "false"}]`, 'utf8');
-
+fs.writeFileSync(file, `[{"trashTalkActive": "false", "hasTrashTalkToday": "false", "isProd": "${isProd}"}]`, 'utf8');
+setTimeout(handleDayEnd, 10000)
 handleInterval();
-setInterval(handleInterval, 1000 * 60 * 5);
+setInterval(handleInterval, timeToInterval);
 
